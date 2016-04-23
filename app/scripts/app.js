2a57ea0c43f16a7861e245715fda58318187d2e5
Fred Spivock <fredspivock@gmail.com>
2016-04-23 18:02:51 -0400

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,40 @@
+var paragraph = document.getElementsByTagName('h2')[0];
+
+function textEffect(animationName) {
+  var text = paragraph.innerHTML,
+		chars = text.length,
+		newText = '',
+		animation = animationName,
+		char,
+		i;
+
+	for (i = 0; i < chars; i += 1) {
+		newText += '<i>' + text.charAt(i) + '</i>';
+	}
+
+	paragraph.innerHTML = newText;
+
+	var wrappedChars = document.getElementsByTagName('i'),
+		wrappedCharsLen = wrappedChars.length,
+		j = 0;
+
+	function addEffect () {
+		setTimeout(function () {
+			wrappedChars[j].className = animation;
+			j += 1;
+			if (j < wrappedCharsLen) {
+				addEffect();
+			}
+		}, 100)
+	}
+
+	addEffect();
+};
+
+textEffect('rainbow');
+
+$('.quiz').on('submit', function () {
+    $(form).append("<h2>You so smart!</h2>")
+    return false;
+});
+
